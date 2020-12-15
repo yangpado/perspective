3b034822cc68aa45a4eac7a9f1e6a24e4de7dc23
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-15 11:18:49 +0900

    content position modify

--- js/custom1.js
+++ js/custom1.js
@@ -39,18 +39,22 @@
             var posX = e.pageX/100;
             var posY = e.pageX/150;
             $('.obj11').css({'left':-270-posX,'bottom':-100-posY});
-            $('.obj12').css({'right':-593-posX,'bottom':-85-posY});
-            $('.obj13').css({'left':50-posX,'bottom':0-posY});
+            $('.obj12').css({'left':-593+posX,'bottom':-85-posY});
+            $('.obj13').css({'left':-50+posX,'bottom':0+posY});
 
             $('.obj21').css({'right':-710-posX,'bottom':-420-posY});
-            $('.obj22').css({'right':-50-posX,'top':-50-posY});
+            $('.obj22').css({'right':-50+posX,'top':-50+posY});
 
             $('.obj31').css({'right':110-posX,'bottom':70-posY});
-            $('.obj32').css({'left':50-posX,'bottom':-160-posY});
+            $('.obj32').css({'left':100-posX,'bottom':-160+posY});
 
-            $('.obj41').css({'left':350-posX,'bottom':-160-posY});
-            $('.obj42').css({'left':50-posX,'bottom':-160-posY});
-            $('.obj43').css({'left':50-posX,'bottom':-160-posY});
+            $('.obj41').css({'left':350+posX,'bottom':-150-posY});
+            $('.obj42').css({'right':167+posX,'bottom':-255-posY});
+            $('.obj43').css({'right':-60+posX,'bottom':-120+posY});
+
+            $('.obj51').css({'left':-100-posX,'bottom':-90-posY});
+            $('.obj52').css({'right':30+posX,'bottom':170-posY});
+            $('.obj53').css({'right':160+posX,'bottom':100-posY});
         })
 
     
